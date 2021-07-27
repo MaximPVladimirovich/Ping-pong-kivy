4375405b77d327f1e8d24b723f7e01670dcfaf0f
maximpvladimirovich <maxim..bleeker@gmail.com>
2021-07-26 22:45:18 -0400

    added bounce off paddles

--- main.py
+++ main.py
@@ -18,23 +18,34 @@ class PongPaddle(Widget):
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
-            speedup = 1.1
-            offset = 0.02 * Vector(0, ball.center_y-self.center_y)
-            ball.velocity = speedup * (offset - ball.velocity)
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
 
 
 # The game rules from pong.kv
 class PingPong(Widget):
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
-    def serve_ball(self):
+    def on_touch_move(self, touch):
+        if touch.x < self.width/3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/3:
+            self.player2.center_y = touch.y
+
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
 
-        # call ball.move and others
+        #paddle bounce
+
         # bounce off top and bottom
         if (self.ball.y < 0 ) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
@@ -50,7 +61,6 @@ class PongBall(Widget):
     # velocity of the ball on the x and y axis
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
-
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
