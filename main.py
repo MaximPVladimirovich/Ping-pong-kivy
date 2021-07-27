266931c8d395af3250abc2483c93e1cf035f9313
maximpvladimirovich <maxim..bleeker@gmail.com>
2021-07-26 20:57:33 -0400

    added ball widget to center of screen

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import kivy
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+
 kivy.require('2.0.0')
 
 # Base class of application inherits from App class
@@ -8,6 +11,17 @@ from kivy.uix.widget import Widget
 
 class PingPong(Widget):
     pass
+
+
+class PongBall(Widget):
+    # velocity of the ball on the x and y axis
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 # Define base class of App
